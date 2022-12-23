efdb3749f7eb195d581c9d7f2d2ca60bd22e6f9b
Lucas G <ghizzolucas@gmail.com>
2022-12-22 23:22:21 -0300

    Initial commit

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
-const form = document.getElementById("form")
-const errorMessage = document.getElementById("error-message")
-
-function validaFormulario(a, b) {
-    return a > b
-}
-
-form.addEventListener('submit', (e) => {
-    const numeroA = document.getElementById("numeroA").value;
-    const numeroB = document.getElementById("numeroB").value;
-
-    errorMessage.classList.remove("valid", "invalid")
-
-    e.preventDefault()
-    if (validaFormulario(numeroA, numeroB)) {
-        errorMessage.classList.add("valid")
-        errorMessage.innerHTML = "<p>O primeiro número é maior que o segundo.</p>"
-    } else {
-        errorMessage.classList.add("invalid")
-        errorMessage.innerHTML = "<p>O primeiro número precisa ser maior que o segundo.</p>"
-    }
-})
-
-errorMessage.addEventListener('click', () => {
-    errorMessage.innerHTML = ''
-    errorMessage.classList.remove("valid", "invalid")
+$(document).ready((e) => {
+    $('form').submit((e) => {
+        e.preventDefault()
+        $(`<li>${$('#tarefa').val()}</li>`).appendTo('ol')
+        
+        $('ol li').click((e) => {
+            e.target.style.textDecoration = "line-through"
+            e.target.style.color = "rgba(0, 179, 0, 0.5)"
+        })
+    })
 })
