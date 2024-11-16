09ff2869a6ea844572b7079bdb550b322c0a745c
Lucas G. <ghizzolucas@gmail.com>
2024-11-15 21:00:37 -0300

    Criado testes de inclusão, alteração e exclusão através do Cypress

---
+++ cypress/e2e/agenda-de-contatos/agenda.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('Testa as funcionalidades da aplicação "Agenda de Contatos"', () => {
+    beforeEach(() => {
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+    })
+
+    it('Teste de inclusão', () => {
+        cy.get('input[type="text"]').type('João da Silva')
+        cy.get('input[type="email"]').type('meu@email.com')
+        cy.get('input[type="tel"]').type('48 99999-9999')
+        cy.get('button[type="submit"]').click()
+        cy.contains('João da Silva')
+    })
+
+    it('Teste de alteração', () => {
+        cy.get('button[class="edit"]').first().click()
+        cy.get('input[type="text"]').clear().type('Nome alterado')
+        cy.get('input[type="email"]').clear().type('alterado@email.com')
+        cy.get('input[type="tel"]').clear().type('48 11111-1111')
+        cy.get('button[type="submit"][class="alterar"]').click()
+        cy.contains('Nome alterado')
+    })
+
+    it('Teste de exclusão', () => {
+        cy.get('.contato').should("have.length.at.least", 1)
+        cy.get('button[class="delete"]', {timeout: 2000}).click({multiple: true})
+        cy.get('.contato').should("have.length", 0)
+    })
+})
